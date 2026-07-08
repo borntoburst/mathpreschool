67de5040038d798841075507d0a2d83121c6741c
borntoburst <minhthangnguyen2109@gmail.com>
2026-07-08 17:11:37 +0700

    Create section_config.py

---
+++ MathPreSchool/models/section_config.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class SectionConfig:
+
+    title: str
+
+    generator: object
+
+    count: int
+
+    priority: int = 3
+
+    color: str = "#FFFFFF"
+
+    icon: str = ""
