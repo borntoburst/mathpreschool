fb2f67dbc8af8e2a61756a6d5d9f74592d3b66c6
borntoburst <minhthangnguyen2109@gmail.com>
2026-07-15 18:16:21 +0700

    Add generator and count fields to SectionConfig

--- MathPreSchool/models/section_config.py
+++ MathPreSchool/models/section_config.py
@@ -1,12 +1,17 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class SectionConfig:
 
     title: str
+
     generator: object
+
     count: int
 
     priority: int = 3
+
     color: str = "#FFFFFF"
+
     icon: str = ""
